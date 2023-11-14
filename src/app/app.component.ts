baec73e33b8c12561a185bc0d75d97d228e4c157
horizoncarlo <carlo.gug@gmail.com>
2023-11-14 00:06:42 -0700

    Added sorting. Added better height. Removed Apple icon to save the load. Put a bunch of neat options on the data table, like resizable columns

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { ConfirmationService, PrimeNGConfig } from 'primeng/api';
+import { ConfirmationService, PrimeNGConfig, SortEvent } from 'primeng/api';
 import { Thing } from './model/thing';
 import { TemplateService } from './service/template.service';
 import { ThingService } from './service/thing.service';
@@ -60,5 +60,42 @@ export class AppComponent implements OnInit {
   
   hasSelectedRows(): boolean {
     return Utility.hasItems(this.selectedRows);
+  }
+  
+  customSort(event: SortEvent) {
+    if (event && event.data && event.field) {
+      event.data.sort((thing1: any, thing2: any) => {
+        const field: string = event.field as string;
+        const value1 = thing1[field];
+        const value2 = thing2[field];
+        
+        let result = null;
+        
+        // Get all our missing cases out of the way
+        if (value1 === null && value2 !== null) {
+          result = -1;
+        }
+        else if (value1 !== null && value2 === null) {
+          result = 1;
+        }
+        else if (value1 === null && value2 === null) {
+          result = 0;
+        }
+        // Can handle strings in a basic way
+        else if (typeof value1 === 'string' && typeof value2 === 'string') {
+          result = value1.localeCompare(value2);
+        }
+        // Handle Fields specifically, as we want to use their string version
+        else if (field === 'fields') {
+          result = (thing1 as Thing).getFieldsAsString().localeCompare((thing2 as Thing).getFieldsAsString());
+        }
+        // Fallback to a direct compare
+        else {
+          result = value1 < value2 ? -1 : value1 > value2 ? 1 : 0;
+        }
+        
+        return (event.order || 0) * result;
+      });
+    }
   }
 }

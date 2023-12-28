3379cb11aabb77923a98cfda0747685e826b21ff
horizoncarlo <carlo.gug@gmail.com>
2023-12-28 14:19:24 -0700

    Progress on login and authTokens and user profile dialog

---
+++ src/app/model/user-auth.ts
@@ -0,0 +1,41 @@
+import { Utility } from "../util/utility";
+
+export class UserAuth {
+  username: string | null = null;
+  authToken: string | null = null;
+  isLoggedIn: boolean = false;
+  
+  checkStoredLogin(): void {
+    // TODO Should probably add a new storage service call to submit an existing authToken and get a fresh one, so that we sometimes nullify old tokens. Trying to avoid the tedium of token expiry and auto-refreshing
+    const possibleAuthToken = Utility.getLocalStorageItem(Utility.LS_AUTH_TOKEN);
+    const possibleUsername = Utility.getLocalStorageItem(Utility.LS_AUTH_USERNAME);
+    if (Utility.isValidString(possibleAuthToken) &&
+        Utility.isValidString(possibleUsername)) {
+      this.username = possibleUsername;
+      this.setLoggedIn(possibleAuthToken as string, true);
+    }
+  }
+  
+  setLoggedIn(authToken: string, saveLogin?: boolean): void {
+    this.authToken = authToken;
+    this.isLoggedIn = true;
+    
+    if (saveLogin) {
+      Utility.setLocalStorageItem(Utility.LS_AUTH_TOKEN, authToken);
+      if (Utility.isValidString(this.username)) {
+        Utility.setLocalStorageItem(Utility.LS_AUTH_USERNAME, this.username as string);
+      }
+    }
+    else {
+      Utility.removeLocalStorageItem(Utility.LS_AUTH_TOKEN);
+      Utility.removeLocalStorageItem(Utility.LS_AUTH_USERNAME);
+    }
+  }
+  
+  setLoggedOut(): void {
+    this.authToken = null;
+    this.isLoggedIn = false;
+    Utility.removeLocalStorageItem(Utility.LS_AUTH_TOKEN);
+    Utility.removeLocalStorageItem(Utility.LS_AUTH_USERNAME);
+  }
+}
---
+++ src/app/user-profile-dialog/user-profile-dialog.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { UserSettings } from '../model/user-settings';
+import { UserService } from '../service/user.service';
+import { Utility } from '../util/utility';
+
+@Component({
+  selector: 'riw-user-profile-dialog',
+  templateUrl: './user-profile-dialog.component.html',
+  styleUrls: ['./user-profile-dialog.component.css']
+})
+export class UserProfileDialogComponent {
+  userService!: UserService;
+  isShowing: boolean = false;
+  settings: UserSettings = new UserSettings();
+  
+  constructor(private incomingUserService: UserService) {
+    this.userService = incomingUserService;
+  }
+  
+  show(): void {
+    // Refresh our settings from our most recent actual object
+    // Note we don't directly use our settings since we want to have the option to Cancel/Save
+    //  whereas the user settings auto-save on change
+    this.settings = UserSettings.cloneFrom(this.userService.getUser());
+    
+    this.isShowing = true;
+  }
+  
+  hide(): void {
+    this.isShowing = false;
+  }
+  
+  submit(): void {
+    // TODO Make forceDial apply dynamically on change here, currently have to refresh the browser
+    this.userService.data$.next(this.settings);
+    Utility.showSuccess('Successfully saved your profile');
+    Utility.fireWindowResize();
+    
+    this.hide();
+  }
+}

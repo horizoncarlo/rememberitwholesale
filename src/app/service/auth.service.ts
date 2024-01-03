77e33bf9d218a61204ca6163c4b89ab0fecdbdb1
horizoncarlo <carlo.gug@gmail.com>
2024-01-02 23:35:32 -0700

    Split Auth into it's own service instead of glomming with User. Also added token checks for every endpoint with some Express middleware. And a minor fix to both the date in the main table and the favorite time saving

---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+import { UserAuth } from "../model/user-auth";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private _auth: UserAuth = new UserAuth();
+  
+  constructor(private router: Router) { }
+  
+  getAuth(): UserAuth {
+    // If this is our first attempt, try to restore an auth key from local storage
+    if (!this._auth.hasCheckedStorage) {
+      this._auth.checkStoredLogin().then(() => {
+        this.router.navigate(['/']);
+      });
+    }
+
+    return this._auth;
+  }
+  
+  performLogout(): void {
+    this._auth.setLoggedOut();
+    
+    this.router.navigate(['/login']);
+  }
+}
